9e81c9e64853ca04ba0db67ba65af1584cc5a819
rnsultanbekov <sultanbekov-01@yandex.ru>
2025-10-27 23:58:54 +0300

    added db init

--- main.go
+++ main.go
@@ -4,18 +4,33 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/ezfroze/go_final_project/pkg/db"
 )
+
+var dbFile = "scheduler.db"
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./web")))
 
 	PORT := os.Getenv("TODO_PORT")
+	TODO_DBFILE := os.Getenv("TODO_DBFILE")
 
 	if PORT == "" {
 		PORT = "7540"
 	}
 
-	err := http.ListenAndServe(fmt.Sprintf(":%s", PORT), nil)
+	if TODO_DBFILE == "" {
+		TODO_DBFILE = dbFile
+	}
+
+	err := db.Init(TODO_DBFILE)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = http.ListenAndServe(fmt.Sprintf(":%s", PORT), nil)
 	if err != nil {
 		panic(err)
 	}
